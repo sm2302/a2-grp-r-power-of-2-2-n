7ce6fa0f189c3dea2f6303bbb657d42338028f53
firdausazizi <17b2196@ubd.edu.bn>
2022-10-28 15:08:34 +0800

    add code for method B

--- solutions.R
+++ solutions.R
@@ -13,15 +13,30 @@ get_theta_by_rand_endpoint <- function(N = 1) {
   return(adiff/2)
 }
 
+get_theta_by_rand_radial_points <- function(N) {
+  # d <- runif(N, 0, radius)
+  # theta <- acos(d / radius)
+  
+  # simplify to:
+  
+  theta <- acos(runif(N, 0, 1))
+  return(theta)
+}
+
 samples <- data.frame(
-  radius = runif(N, 1, 100),
-  theta = get_theta_by_rand_endpoint(N)
+  radius = runif(N * 2, 1, 100),
+  theta = c(
+    get_theta_by_rand_endpoint(N), 
+    get_theta_by_rand_radial_points(N)
+  )
 )
 
 chord_length <- 2 * sin(samples$theta) * samples$radius
 
 triangle_length <- sqrt(3) * samples$radius
 
-mean(chord_length > triangle_length) # 0.333
+mean(chord_length[1:N] > triangle_length[1:N]) # 0.333
+mean(chord_length[(N+1):(2*N)] > triangle_length[(N+1):(2*N)]) #0.5
 
 # For method A, Pr(ch_len > tr_len) is 0.333
+# For method B, Pr(ch_len > tr_len) is 0.5
